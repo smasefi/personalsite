23616f81e543d01ab8b2704a2fc1669fc6faa60c
smasefi <samanthamilan@gmail.com>
2024-06-23 21:34:47 -0400

    work cards

--- js/script.js
+++ js/script.js
@@ -16,3 +16,16 @@ document.addEventListener("DOMContentLoaded", function() {
 
 });
 
+var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'));
+var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
+    return new bootstrap.Popover(popoverTriggerEl);
+});
+
+document.addEventListener('click', function (e) {
+    var target = e.target;
+    if (!target.matches('[data-bs-toggle="popover"]') && !target.closest('.popover')) {
+        popoverList.forEach(function (popover) {
+            popover.hide();
+        });
+    }
+});
